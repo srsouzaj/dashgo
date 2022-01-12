7d2f284cd4f24aedc9f34803853c732904251938
Sr. Souza <jorgejrdj1@gmail.com>
2022-01-12 15:48:56 -0300

    adicionando o gráfico

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,20 +1,109 @@
-import { Flex } from "@chakra-ui/react"
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from 'next/dynamic';
+import { ApexOptions } from 'apexcharts';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+}); 
+
 import Header from "../components/Header";
-import Sidebar from '../components/Sidebar';
+import Sidebar from "../components/Sidebar";
 
-export default function dashboard() {
-    return (
+const options : ApexOptions  = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2022-01-12T00:00:00.000z',
+      '2022-01-13T00:00:00.000z',
+      '2022-01-14T00:00:00.000z',
+      '2022-01-15T00:00:00.000z',
+      '2022-01-16T00:00:00.000z',
+      '2022-01-17T00:00:00.000z',
+      '2022-01-18T00:00:00.000z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    }
+  }
+}
 
-        <Flex direction="column" h="100vh">
-            <Header />
-            <Flex
-                w="100%"
-                my="6"
-                maxWidth={1480}
-                mx="auto"
-                px="6">
-                <Sidebar />
-            </Flex>
-        </Flex>
-    )
+const series = [
+  {
+    name: 'series1', 
+    data: [31, 120, 10, 28, 61, 18, 109]
+  }
+]
+
+export default function Dashboard() {
+  return (
+    <Flex direction='column' h='100vh'>
+      <Header />
+
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+        <Sidebar />
+
+        <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
+          <Box
+            p={['6', '8']}
+            bg='gray.800'
+            borderRadius='8'
+            pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
+            <Chart
+              options={options}
+              series={series} 
+              type='area'
+              height={160} 
+            />
+          </Box>
+
+          <Box
+            p={['6', '8']}
+            bg='gray.800'
+            borderRadius='8'
+            pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+            <Chart
+              options={options}
+              series={series} 
+              type='area'
+              height={160} 
+            />
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  )
 }
