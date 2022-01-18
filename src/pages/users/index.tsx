c608f95a9cb42abebd524f949f496bffd63586a4
@Sr. Souza, Jorge <jorgejrdj1@gmail.com>
2022-01-18 19:43:55 -0300

    Verificando se o link está ativo ou não

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,7 +14,8 @@ import {
     Text,
     useBreakpointValue
 } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import Link from "next/link";
+import { RiAddLine } from "react-icons/ri";
 
 import Header from '../../components/Header';
 import { Pagination } from "../../components/Pagination";
@@ -35,16 +36,17 @@ export default function UserList() {
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
-
-                        <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="teal"
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar Novo
-                        </Button>
+                        <Link href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="teal"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar Novo
+                            </Button>
+                        </Link>
                     </Flex>
 
                     <Table colorScheme="whiteAlpha">
