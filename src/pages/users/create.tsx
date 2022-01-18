1f71d8d1e2d7c129055f2c01356d54c8c6a8c8eb
@Sr. Souza, Jorge <jorgejrdj1@gmail.com>
2022-01-18 19:16:19 -0300

    create responsivo

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -21,17 +21,17 @@ import {
         <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
           <Sidebar />
   
-          <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Box flex="1" borderRadius={8} bg="gray.800" p={["6","8"]}>
             <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
   
             <Divider my="6" borderColor="gray.700" />
   
             <VStack spacing="8">
-              <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
                 <Input name="name" label="Nome Completo" />
                 <Input name="email" label="E-mail" />
               </SimpleGrid>
-              <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
                 <Input name="password" type="password" label="Senha" />
                 <Input name="password_confirmation" type="password" label="Confirmar Senha" />
               </SimpleGrid>
