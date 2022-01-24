0e0d5c40d3c36a6a4a9f7f0cb3879b2f51009011
@Sr. Souza, Jorge <jorgejrdj1@gmail.com>
2022-01-24 11:07:15 -0300

    criando useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,33 +18,14 @@ import {
 import Link from "next/link";
 import { RiAddLine } from "react-icons/ri";
 
-import { useQuery } from "react-query";
 
 import Header from '../../components/Header';
 import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const { data } = await api.get('users')
-        
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-                    day: '2-digit',
-                    month: "long",
-                    year: 'numeric'
-                })
-            };
-        });
-
-        return users;
-
-    }, { staleTime: 1000 * 5 })
+    const { data, isLoading, error } = useUsers();
 
     const isWideVersion = useBreakpointValue({
         base: false,
