d01ff4d75a027999b11d7ae256f49bd0a7d27eca
@Sr. Souza, Jorge <jorgejrdj1@gmail.com>
2022-01-24 14:21:13 -0300

    navegação por paginação

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,9 +3,10 @@ import { Button } from "@chakra-ui/react";
 interface PaginationItemProps {
     number: number;
     isCurrent?: boolean;
+    onPageChange: (page: number) => void;
 }
 
-export function PaginationItem({ isCurrent = false, number }
+export function PaginationItem({ isCurrent = false, onPageChange, number }
     : PaginationItemProps) {
     if (isCurrent) {
         return (
@@ -34,6 +35,8 @@ export function PaginationItem({ isCurrent = false, number }
             _hover={{
                 bg: 'gray.500'
             }}
+
+            onClick={() => onPageChange(number)}
         >
             {number}
         </Button>
